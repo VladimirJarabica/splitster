b92a7b2ad11460ba5217cf9be5775756bce785b5
VladimirJarabica <vladojarabica@gmail.com>
2017-09-06 08:23:34 +0200

    client app

--- examples/server-app/src/app.js
+++ examples/server-app/src/app.js
@@ -1,15 +1,15 @@
-import express from "express"
-import { server as splitsterInit } from "../../../lib/main"
+import express from 'express'
+import { server as splitsterInit } from '../../../src/main'
 
-import config from "./config"
+import config from './config'
 
 const app = express()
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const user = {
-    language: "en",
-    os: "windows",
-    device: "chrome",
+    language: 'en',
+    os: 'windows',
+    device: 'chrome',
     location: {},
   }
 
@@ -18,9 +18,9 @@ app.get("/", (req, res) => {
 
   splitster.runAll()
 
-  res.json(splitster.getState())
+  res.json(splitster.getSaveResults())
 })
 
 app.listen(3000, () => {
-  console.log("Server client app running on port 3000!")
+  console.log('Server client app running on port 3000!')
 })
